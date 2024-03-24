1fe13460a55bd1be0c59d35d4498ab9c89ad9cd9
carla <carla@LAPTOP-15A817UA>
2024-03-24 15:43:02 -0300

    Adicionar arquivos de projeto.

---
+++ Produto.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_001
+{
+    public class produto
+    {
+        public static double calcular(DateTime AquisicaoProduto, double ValorProduto)
+        {
+            double resultado = 0;
+            try
+            {
+                int mes = AquisicaoProduto.Month;
+                if (mes >= 1 && mes <= 7)
+                {
+                    resultado = ValorProduto - ValorProduto * 0.30;
+                }
+                else
+                {
+                    resultado = ValorProduto - ValorProduto * 0.50;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            return resultado;
+        }
+
+        public static double calcularAcrescimo(string OrigemProduto, double ValorProduto)
+        {
+            double resultado = 0;
+            try
+            {
+                switch (OrigemProduto) 
+                {
+                    case "1":
+                        resultado = ValorProduto * 0.10;
+                        break;
+                    case "2":
+                        resultado = ValorProduto * 0.20;
+                        break;
+                    case "3":
+                        resultado = ValorProduto * 0.30;
+                        break;
+                    case "4":
+                        resultado = ValorProduto * 0.40;
+                        break;
+                    case "5":
+                        resultado = ValorProduto * 0.50;
+                        break;
+                                             
+
+                }
+            }
+            catch (Exception ex)
+            { Console.WriteLine(ex); }
+
+            return resultado;
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using Projeto_001;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Programa.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string NomeProduto = "";
+            DateTime dataValidade, AquisicaoProduto;
+            string OrigemProduto = "";
+            double ValorProduto = 0;
+            int QuantidadeProduto = 0;
+
+            Console.WriteLine("Informe o nome do produto:\n");
+            NomeProduto = Console.ReadLine();
+
+            Console.WriteLine("Informe a data de validade do produto (DD/MM/AAAA):\n");
+            dataValidade = Convert.ToDateTime(Console.ReadLine());
+
+            Console.WriteLine("Informe a origem do produto: \n 1 - Sudeste \n 2 - Centro-Oeste \n 3 - Norte \n 4 - Sul \n 5 - Nordeste \n");
+            OrigemProduto = Console.ReadLine();
+
+            Console.WriteLine($"Origem do produto: {OrigemProduto}");
+
+            Console.WriteLine("Informe o valor do produto em reais (R$ xx,xx):\n");
+            ValorProduto = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Insira quantas unidades de produto foram adquiridas:\n");
+            QuantidadeProduto = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("Informe o mês e ano de aquisição do produto (MM/AAAA):\n");
+            AquisicaoProduto = Convert.ToDateTime(Console.ReadLine());
+
+            double resultadocalculo = produto.calcular(AquisicaoProduto, ValorProduto);
+            Console.WriteLine($"O desconto aplicado em cada produto é: {resultadocalculo}");
+
+            double resultadoacrescimo = produto.calcularAcrescimo(OrigemProduto, ValorProduto);
+            Console.WriteLine($"O acrescimo aplicado em cada produto é: {resultadoacrescimo}");
+
+
+        }
+
+    }
+}
